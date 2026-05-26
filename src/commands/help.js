fa4267120a0ae3464861de6a8af4d7b61d881770
cunderhill17 <93687857+cunderhill17@users.noreply.github.com>
2026-05-26 16:49:58 -0400

    feat: update command menu with 'empty trash' option

--- src/commands/help.js
+++ src/commands/help.js
@@ -6,11 +6,12 @@ async function showHelp({userCommand}) {
 console.log(`\nCOMMAND MENU:
 =============
 
-move:       moves files from one directory to another based on user inputed extension
-list:       lists files in the users current working directory
-delete:     deletes files by name or extension
-restore:    restores files from tash bin to current working directory
-exit:       closes the program (all changes made will be final)\n`);
+move:           moves files from one directory to another based on user inputed extension
+list:           lists files in the users current working directory
+delete:         deletes files by name or extension
+restore:        restores files from tash bin to current working directory
+empty trash:    permenately deletes all files in the trash bin
+exit:           closes the program\n`);
 
     const [newCommand, ...unusedInput] = await prompt('noCommand');
     !newCommand ? userCommand('exit') : userCommand(newCommand);
